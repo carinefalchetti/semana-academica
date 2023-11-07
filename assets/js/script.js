f676663637a26f2ee5b4267f763728d6eaa17228
Carine <142498633+carinefalchetti@users.noreply.github.com>
2023-11-07 18:34:25 +0000

    ghj

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
-    $('.carrossel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
+  $('.carrossel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
 });
 
 function mascara(i) {
-  var v = i.value;
+var v = i.value;
 
-  if(isNaN(v[v.length-1])){
-      i.value = v.substring(0, v.length - 1);
-      return;
-  }
-
-  i.setAttribute("maxlength", "14");
-  if (v.length == 3 || v.length == 7) i.value += ".";
-  if (v.length == 11) i.value += ".";
+if(isNaN(v[v.length-1])){
+    i.value = v.substring(0, v.length - 1);
+    return;
 }
+
+i.setAttribute("maxlength", "14");
+if (v.length == 3 || v.length == 7) i.value += ".";
+if (v.length == 11) i.value += ".";}
 
 function validarFormulario() {
     var nome = document.querySelector("input[name='nome']").value;
